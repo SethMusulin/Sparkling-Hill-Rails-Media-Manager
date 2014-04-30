e1645d1df9c51ded77f7d7b9b5ab5f620e8fa719
Cara Dennis <Cara@https://github.com/ephemere>
2014-04-30 16:16:59 -0600

    [#70292378] A user can create a picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,12 @@
 class Picture < ActiveRecord::Base
-
+  validates :url,
+            length: {
+              minimum: 4,
+              message: "URL cannot be blank"
+            }
+  validates :description,
+            length: {
+              minimum: 3,
+              message: "Description cannot be blank"
+            }
 end
