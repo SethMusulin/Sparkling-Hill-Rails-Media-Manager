17a51d17b6a3d9adfe4b659d4b349ecb74cfe60a
Cara Dennis <Cara@https://github.com/ephemere>
2014-05-01 10:39:10 -0600

    refactored blank form submission on pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,12 +1,4 @@
 class Picture < ActiveRecord::Base
-  validates :url,
-            length: {
-              minimum: 4,
-              message: "URL cannot be blank"
-            }
-  validates :description,
-            length: {
-              minimum: 3,
-              message: "Description cannot be blank"
-            }
+  validates :url, presence: true
+  validates :description, presence: true
 end
